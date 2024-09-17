9a66f93419787c19de999d52adb94e04955f34f9
BT_2S14_21 <michaelk368@nycstudents.net>
2024-09-17 10:53:13 -0400

    Created the base program with complete functionality and symbols/formatting. Still need to include comments and make it look nicer.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,41 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         System.out.println("Welcome to the Tip Calculator (Version 1.0)");
         System.out.println("------------------------------------");
+
         Scanner s = new Scanner(System.in);
         System.out.print("Bill Amount: $");
         String initialBill = s.nextLine();
         double initialBillConv = Double.parseDouble(initialBill);
+
         System.out.print("Percent Tip (Whole Number): ");
         String tipNum = s.nextLine();
         double tipNumConv = Double.parseDouble(tipNum)/100;
+
         System.out.print("Number of People Paying: ");
         String numPeople = s.nextLine();
+
         int numPeopleConv = Integer.parseInt(numPeople);
-        double tipTotal = ((initialBillConv*tipNumConv));
+        double tipTotal = (tipNumConv*initialBillConv);
         double finalBill = (initialBillConv+tipTotal);
-        double tipEach = (tipTotal/numPeopleConv);
-        double personPay = (finalBill/numPeopleConv);
-        System.out.println();
+        double tipEachPerson = (tipTotal/numPeopleConv);
+        double eachPersonPay = (finalBill/numPeopleConv);
 
-        System.out.println(tipTotal);
+        System.out.println("-----------------------");
+        System.out.println("Total Bill Per Person: $" + String.format("%.2f", eachPersonPay));
+        System.out.println("-----------------------");
+        System.out.println("Tip Per Person: $" + String.format("%.2f", tipEachPerson));
+        System.out.println("-----------------------");
+        System.out.println("Total Tip: $" + String.format("%.2f", tipTotal));
+        System.out.println("-----------------------");
+        System.out.println("Final Bill: $" + String.format("%.2f", finalBill));
         s.close();
-
 
     }
 }
