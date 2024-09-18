ca1e35c5f76c8320839ef68b1b77e9928c60c7fa
BT_2S14_21 <michaelk368@nycstudents.net>
2024-09-18 10:10:45 -0400

    - Added comments - Made program easier to use/understand - Variables are named better

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Welcome to the Tip Calculator (Version 1.0)");
+        System.out.println("Welcome to the Tip Calculator (Version 1.1)");
+        System.out.println("Do NOT type any symbols when using the calculator!");
         System.out.println("------------------------------------");
 
         Scanner s = new Scanner(System.in);
         System.out.print("Bill Amount: $");
-        String initialBill = s.nextLine();
-        double initialBillConv = Double.parseDouble(initialBill);
+        String initialBill = s.nextLine(); // The bill before any tip calculations
+        double initialBillConv = Double.parseDouble(initialBill); // Converts the string into a double for parsing
 
-        System.out.print("Percent Tip (Whole Number): ");
-        String tipNum = s.nextLine();
-        double tipNumConv = Double.parseDouble(tipNum)/100;
+        System.out.print("Percent Tip: ");
+        String tipPercent = s.nextLine(); // The tip to be applied
+        double tipPercentConv = Double.parseDouble(tipPercent)/100; // Whole number tip is divided by 100 for easier calculations
 
         System.out.print("Number of People Paying: ");
         String numPeople = s.nextLine();
 
-        int numPeopleConv = Integer.parseInt(numPeople);
-        double tipTotal = (tipNumConv*initialBillConv);
-        double finalBill = (initialBillConv+tipTotal);
-        double tipEachPerson = (tipTotal/numPeopleConv);
-        double eachPersonPay = (finalBill/numPeopleConv);
+        int numPeopleConv = Integer.parseInt(numPeople); // numPeople is initialized as an int
+        double tipTotal = (tipPercentConv*initialBillConv); // The total tip is equal to the tip applied multiplied by the bill before any tip
+        double finalBill = (initialBillConv+tipTotal); // The final bill is equal to the bill before any tip plus the tip
+        double tipEachPerson = (tipTotal/numPeopleConv); // The amt each person pays in tips
+        double eachPersonPay = (finalBill/numPeopleConv); // The total amt each person should pay
 
-        System.out.println("-----------------------");
-        System.out.println("Total Bill Per Person: $" + String.format("%.2f", eachPersonPay));
-        System.out.println("-----------------------");
+        System.out.println("----------------------------------------------");
+        System.out.println("Total Bill Per Person: $" + String.format("%.2f", eachPersonPay)); // This stat is displayed first because it is the most useful
+        System.out.println("----------------------------------------------");
         System.out.println("Tip Per Person: $" + String.format("%.2f", tipEachPerson));
-        System.out.println("-----------------------");
+        System.out.println("----------------------------------------------");
         System.out.println("Total Tip: $" + String.format("%.2f", tipTotal));
-        System.out.println("-----------------------");
-        System.out.println("Final Bill: $" + String.format("%.2f", finalBill));
-        s.close();
+        System.out.println("----------------------------------------------");
+        System.out.println("Final Bill: $" + String.format("%.2f", finalBill)); // This is displayed at the end because it is the final bill; makes the most sense
+        System.out.println("----------------------------------------------");
+        s.close(); // Stops the scanner s to prevent memory leak
 
     }
 }
